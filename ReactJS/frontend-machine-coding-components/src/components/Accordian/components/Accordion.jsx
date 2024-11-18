acb03bae8eed095e59ea9f328106fe062e86d7d0
pandeyswastik23 <pandeyswastik23@gmail.com>
2024-11-18 18:01:46 +0530

    removed accrdion css

--- ReactJS/frontend-machine-coding-components/src/components/Accordian/components/Accordion.jsx
+++ ReactJS/frontend-machine-coding-components/src/components/Accordian/components/Accordion.jsx
@@ -1,6 +1,5 @@
 import { AccordionProvider } from "../context/Accordion.Context";
 import AccordionList from "./AccordionList";
-import "../css/Accordion.css";
 
 const Accordion = () => {
   return (
